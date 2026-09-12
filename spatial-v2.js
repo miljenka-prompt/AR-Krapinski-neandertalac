d9d25bf77d2ef5aeb0f35d32cc83faa9b4e1a736
Miljenka Ćurković <miljenka.cur@gmail.com>
2026-09-12 06:37:24 -0700

    Always refresh Neanderthal mask during alpha cleanup

--- spatial-v2.js
+++ spatial-v2.js
@@ -3,7 +3,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.183.2/build/three.m
 window.THREE = THREE
 
 const VIDEO_URL = './Krapinski_neandertalac.mp4?v=20260912c'
-const MASK_URL = '/QInspired-WebAR-Tracking-Test/neanderthal-mask.mp4?v=20260912c'
+const MASK_URL = `/QInspired-WebAR-Tracking-Test/neanderthal-mask.mp4?rev=${Date.now()}`
 
 let rgbVideo = null
 let maskVideo = null
